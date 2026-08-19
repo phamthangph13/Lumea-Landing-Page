cd7474778eaf8d1d99b4f99c31838ca6ee36527e
phamthangph13 <winnieph13@gmail.com>
2026-08-19 12:17:10 +0700

    fix(font): remove Japanese palt feature setting and load complete Vietnamese font weights

--- nuxt.config.js
+++ nuxt.config.js
@@ -83,7 +83,7 @@ export default {
       },
       {
         rel: 'stylesheet',
-        href: 'https://fonts.googleapis.com/css2?family=Barlow+Condensed:ital,wght@0,600;0,700;0,800;0,900;1,700&family=Plus+Jakarta+Sans:ital,wght@0,400;0,500;0,600;0,700;1,400&display=swap'
+        href: 'https://fonts.googleapis.com/css2?family=Barlow+Condensed:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,700&family=Plus+Jakarta+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,400;1,700&display=swap'
       }
     ],
     bodyAttrs: {
@@ -154,7 +154,7 @@ export default {
 
   webfontloader: {
     google: {
-      families: ['Barlow+Condensed:600,700,800,900:latin,vietnamese', 'Plus+Jakarta+Sans:400,500,600,700:latin,vietnamese']
+      families: ['Barlow+Condensed:300,400,500,600,700,800,900:latin,vietnamese', 'Plus+Jakarta+Sans:300,400,500,600,700,800:latin,vietnamese']
     }
   },
 
